9946c728024b340e79875d1f3410ced7ea8f60e7
brendadosanjos <brendakelly_oliveira@live.com>
2024-10-08 12:17:24 -0300

    finalização do exercicio 1

--- exercicio 1/script.js
+++ exercicio 1/script.js
@@ -1,2 +1,11 @@
-var elementos = document.querySelectorAll('*') //ou getElementsByTagName('*')
-document.write(elementos.length)
+let elementos = document.querySelectorAll('*') //ou getElementsByTagName('*')
+document.write(`quantidade de elementos ${elementos.length}`)
+
+let todosElementos =""
+
+for(e of elementos){
+    todosElementos = todosElementos + "<br>" + e.tagName
+}
+
+document.write(todosElementos)
+
